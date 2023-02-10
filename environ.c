09e36d7826ad607eb62b3b7ddc64cd3a9e4e4318
Dawit Tadesse <dawittadesse927@gmail.com>
2023-02-10 16:35:08 +0300

    done with environs

--- environ.c
+++ environ.c
@@ -13,6 +13,28 @@ int _myenv(info_t *info)
 }
 
 /**
+ * _getenv - gets the value of an environ variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: env var name
+ *
+ * Return: the value
+ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
  * _mysetenv - Initialize a new environment variable,
  *             or modify an existing one
  * @info: Structure containing potential arguments. Used to maintain
@@ -49,5 +71,21 @@ int _myunsetenv(info_t *info)
 	for (i = 1; i <= info->argc; i++)
 		_unsetenv(info, info->argv[i]);
 
+	return (0);
+}
+
+/**
+ * populate_env_list - populates env linked list
+ * @info: Structure containing potential arguments.
+ * Return: Always 0
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
 	return (0);
 }
