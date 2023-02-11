ec5b6e471cb0eff08b5c69a94fb279f62e682480
Sweetpanda68 <Mgirma976@gmail.com>
2023-02-11 05:44:45 -0800

    remove_comments method added

--- errors1.c
+++ errors1.c
@@ -121,3 +121,20 @@ char *convert_number(long int num, int base, int flags)
 	return (ptr);
 }
 
+/**
+ * remove_comments - function replaces first instance of '#' with '\0'
+ * @buf: address of the string to modify
+ *
+ * Return: Always 0;
+ */
+void remove_comments(char *buf)
+{
+	int i;
+
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+		{
+			buf[i] = '\0';
+			break;
+		}
+}
