01f88bb15865a0dcd07cabf10d74a34d388ebe39
Sweetpanda68 <Mgirma976@gmail.com>
2023-02-11 04:55:04 -0800

    str_cat method

--- string.c
+++ string.c
@@ -54,3 +54,22 @@ char *starts_with(const char *haystack, const char *needle)
 			return (NULL);
 	return ((char *)haystack);
 }
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ * @src: the source buffer
+ *
+ * Return: pointer to destination buffer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
