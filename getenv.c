08c12927f87a733f819b7fba39ba04f073c98d43
Sweetpanda68 <Mgirma976@gmail.com>
2023-02-11 05:55:56 -0800

    get environment DONE

--- getenv.c
+++ getenv.c
@@ -17,3 +17,34 @@ char **get_environ(info_t *info)
 	return (info->environ);
 }
 
+/**
+ * _unsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: 1 on delete, 0 otherwise
+ * @var: the string env var property
+ */
+int _unsetenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
