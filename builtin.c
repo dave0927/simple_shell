f6eb481f9e6fa8932a80750923a289b2e441e03a
Sweetpanda68 <Mgirma976@gmail.com>
2023-02-13 03:05:10 -0800

    _myhelp

--- builtin.c
+++ builtin.c
@@ -79,3 +79,19 @@ int _mycd(info_t *info)
 	return (0);
 }
 
+/**
+ * _myhelp - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _myhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
+}
