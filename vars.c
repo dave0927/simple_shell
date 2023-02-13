f1239eda02a0353c744dbcea414d9ab455d4ed1b
Dawit Tadesse <dawittadesse927@gmail.com>
2023-02-13 15:56:13 +0300

    vars.c file

---
+++ vars.c
@@ -0,0 +1,10 @@
+#include "shell.h"
+
+/**
+ * is_chain - test if current char in buffer is a chain delimeter
+ * @info: the parameter struct
+ * @buf: the char buffer
+ * @p: address of current position in buf
+ *
+ * Return: 1 if chain delimeter, 0 otherwise
+ */
