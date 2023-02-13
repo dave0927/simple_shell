0516723c00f511944c3ed9aaef0525ca3001214c
Dawit Tadesse <dawittadesse927@gmail.com>
2023-02-13 12:48:42 +0300

    print_list method added

--- lists1.c
+++ lists1.c
@@ -53,3 +53,27 @@ char **list_to_strings(list_t *head)
 	strs[i] = NULL;
 	return (strs);
 }
+
+/**
+ * print_list - prints all elements of a list_t linked list
+ * @h: pointer to first node
+ *
+ * Return: size of list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		_puts(convert_number(h->num, 10, 0));
+		_putchar(':');
+		_putchar(' ');
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
