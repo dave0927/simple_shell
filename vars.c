b548925beb504a02db68ffc995888a74e9aa6c93
Dawit Tadesse <dawittadesse927@gmail.com>
2023-02-13 16:15:20 +0300

    replace_string: replaces string

--- vars.c
+++ vars.c
@@ -138,3 +138,17 @@ int replace_vars(info_t *info)
 	}
 	return (0);
 }
+
+/**
+ * replace_string - replaces string
+ * @old: address of old string
+ * @new: new string
+ *
+ * Return: 1 if replaced, 0 otherwise
+ */
+int replace_string(char **old, char *new)
+{
+	free(*old);
+	*old = new;
+	return (1);
+}
