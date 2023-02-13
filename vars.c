3f5295fe630ae4d48aaa0ba1d66256561b7714cb
Sweetpanda68 <Mgirma976@gmail.com>
2023-02-13 05:02:32 -0800

    check_chain

--- vars.c
+++ vars.c
@@ -35,3 +35,36 @@ int is_chain(info_t *info, char *buf, size_t *p)
 	return (1);
 }
 
+/**
+ * check_chain - checks we should continue chaining based on last status
+ * @info: the parameter struct
+ * @buf: the char buffer
+ * @p: address of current position in buf
+ * @i: starting position in buf
+ * @len: length of buf
+ *
+ * Return: Void
+ */
+void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
+{
+	size_t j = *p;
+
+	if (info->cmd_buf_type == CMD_AND)
+	{
+		if (info->status)
+		{
+			buf[i] = 0;
+			j = len;
+		}
+	}
+	if (info->cmd_buf_type == CMD_OR)
+	{
+		if (!info->status)
+		{
+			buf[i] = 0;
+			j = len;
+		}
+	}
+
+	*p = j;
+}
