75db3850a40027dd34dea6f53e5732e72b16beb6
Dawit Tadesse <dawittadesse927@gmail.com>
2023-02-13 13:07:44 +0300

    _memset

--- realloc.c
+++ realloc.c
@@ -7,3 +7,12 @@
  *@n: the amount of bytes to be filled
  *Return: (s) a pointer to the memory area s
  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
