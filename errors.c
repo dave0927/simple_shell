a37ba9ffae8be59b58b82f0d085c90b6c56dc431
Sweetpanda68 <Mgirma976@gmail.com>
2023-02-13 03:21:21 -0800

    _putsfd

--- errors.c
+++ errors.c
@@ -63,3 +63,23 @@ int _putfd(char c, int fd)
 		buf[i++] = c;
 	return (1);
 }
+
+/**
+ *_putsfd - prints an input string
+ * @str: the string to be printed
+ * @fd: the filedescriptor to write to
+ *
+ * Return: the number of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
