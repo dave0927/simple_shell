13d7c9471750e7592d98b675ebfc40ed57e88fd7
Dawit Tadesse <dawittadesse927@gmail.com>
2023-02-13 15:21:55 +0300

    dup_chars: duplicates characters

--- parser.c
+++ parser.c
@@ -21,3 +21,23 @@ int is_cmd(info_t *info, char *path)
 	}
 	return (0);
 }
+
+/**
+ * dup_chars - duplicates characters
+ * @pathstr: the PATH string
+ * @start: starting index
+ * @stop: stopping index
+ *
+ * Return: pointer to new buffer
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
