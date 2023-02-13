4d1c21941816dbd623ddb15f81d3dfdf7e554553
Sweetpanda68 <Mgirma976@gmail.com>
2023-02-13 03:13:40 -0800

    _eputchar

--- errors.c
+++ errors.c
@@ -19,3 +19,24 @@ void _eputs(char *str)
 	}
 }
 
+/**
+ * _eputchar - writes the character c to stderr
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
