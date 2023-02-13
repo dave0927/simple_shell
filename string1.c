727e2b6a9e8483a7bb3bf47a1cb2de03d429b10b
Dawit Tadesse <dawittadesse927@gmail.com>
2023-02-13 15:44:44 +0300

    _puts - prints an input string

--- string1.c
+++ string1.c
@@ -44,3 +44,23 @@ char *_strdup(const char *str)
 		ret[length] = *--str;
 	return (ret);
 }
+
+/**
+ *_puts - prints an input string
+ *@str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
