2f7659584f40c136cad0f7cf6390776238ae2a64
Sweetpanda68 <Mgirma976@gmail.com>
2023-02-13 01:52:08 -0800

    determines length of linked list

--- lists1.c
+++ lists1.c
@@ -77,3 +77,24 @@ size_t print_list(const list_t *h)
 	return (i);
 }
 
+/**
+ * node_starts_with - returns node whose string starts with prefix
+ * @node: pointer to list head
+ * @prefix: string to match
+ * @c: the next character after prefix to match
+ *
+ * Return: match node or null
+ */
+list_t *node_starts_with(list_t *node, char *prefix, char c)
+{
+	char *p = NULL;
+
+	while (node)
+	{
+		p = starts_with(node->str, prefix);
+		if (p && ((c == -1) || (*p == c)))
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
+}
