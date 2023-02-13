98854f69814435ff4df5195672750bfc27c57353
Dawit Tadesse <dawittadesse927@gmail.com>
2023-02-13 13:06:58 +0300

    fill memory with constant bytes

---
+++ realloc.c
@@ -0,0 +1,9 @@
+#include "shell.h"
+
+/**
+ **_memset - fills memory with a constant byte
+ *@s: the pointer to the memory area
+ *@b: the byte to fill *s with
+ *@n: the amount of bytes to be filled
+ *Return: (s) a pointer to the memory area s
+ */
