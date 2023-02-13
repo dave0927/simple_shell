0ad9e5451e50e3379fdcf00d5d6666fde4c74110
Dawit Tadesse <dawittadesse927@gmail.com>
2023-02-13 15:57:41 +0300

    is_chain: test if buffer is a chain delimeter

--- vars.c
+++ vars.c
@@ -8,3 +8,30 @@
  *
  * Return: 1 if chain delimeter, 0 otherwise
  */
+int is_chain(info_t *info, char *buf, size_t *p)
+{
+	size_t j = *p;
+
+	if (buf[j] == '|' && buf[j + 1] == '|')
+	{
+		buf[j] = 0;
+		j++;
+		info->cmd_buf_type = CMD_OR;
+	}
+	else if (buf[j] == '&' && buf[j + 1] == '&')
+	{
+		buf[j] = 0;
+		j++;
+		info->cmd_buf_type = CMD_AND;
+	}
+	else if (buf[j] == ';') /* found end of this command */
+	{
+		buf[j] = 0; /* replace semicolon with null */
+		info->cmd_buf_type = CMD_CHAIN;
+	}
+	else
+		return (0);
+	*p = j;
+	return (1);
+}
+
