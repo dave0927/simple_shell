1ccf79a15eb74a9efaab160072de7566a412482f
Sweetpanda68 <Mgirma976@gmail.com>
2023-02-13 03:30:40 -0800

    set_info

--- getinfo.c
+++ getinfo.c
@@ -11,3 +11,35 @@ void clear_info(info_t *info)
 	info->path = NULL;
 	info->argc = 0;
 }
+
+/**
+ * set_info - initializes info_t struct
+ * @info: struct address
+ * @av: argument vector
+ */
+void set_info(info_t *info, char **av)
+{
+	int i = 0;
+
+	info->fname = av[0];
+	if (info->arg)
+	{
+		info->argv = strtow(info->arg, " \t");
+		if (!info->argv)
+		{
+
+			info->argv = malloc(sizeof(char *) * 2);
+			if (info->argv)
+			{
+				info->argv[0] = _strdup(info->arg);
+				info->argv[1] = NULL;
+			}
+		}
+		for (i = 0; info->argv && info->argv[i]; i++)
+			;
+		info->argc = i;
+
+		replace_alias(info);
+		replace_vars(info);
+	}
+}
